cafaeb474bcc4d356a73fa1c20484e180e9a3d2b
lif3racer <34902314+oshanadithaya@users.noreply.github.com>
2019-10-24 22:51:39 +0530

    Add files via upload

---
+++ Character_Finder.java
@@ -0,0 +1,11 @@
+package com.oshan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String message = "Hello Sri Lanaka";
+
+        System.out.println(message.length());
+
+    }
+}
